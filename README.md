4634802b981343960fec22b814b07f6579f20df2
Richard Senior <richard.j.senior@gmail.com>
2017-03-18 14:18:08 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello
+
+Template project for command line tools written in C and built
+with `cmake` and `doxygen`.
+
+## Customization Checklist
+
+* Update project name in CMakeLists.txt
+* Review compiler flags in CMakeLists.txt
+* Update target name in src/CMakeLists.txt
+* Add project name to AUTHORS file if required
+* Update main.c as required
+* Update this README file
---
+++ src/main.c
@@ -0,0 +1,75 @@
+/**
+ * @file main.c
+ *
+ * Main program.
+ *
+ * Copyright (c) 2017 Richard Senior
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @cond Doxygen_Suppress
+#define _POSIX_C_SOURCE 200809L
+/// @endcond
+
+#include "main.h"
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Prints a usage message to standard output.
+ */
+static void usage()
+{
+    printf("hello v%s\n", PROJECT_VERSION);
+    puts("Usage: hello [OPTIONS]");
+    puts("  -n, --name   Name used in greeting");
+}
+
+/**
+ * Main program.
+ *
+ * @param argc number of arguments
+ * @param argv program arguments
+ * @return exit status
+ */
+int main(int argc, char **argv)
+{
+    static struct option longopts[] = {
+        {"name", required_argument, NULL, 'n'}
+    };
+
+    int c;
+    char *name = NULL;
+    while ((c = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+        switch (c) {
+        case 'n':
+            if ((name = strdup(optarg)) == NULL) {
+                perror("strdup");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        default:
+            usage();
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+
+    printf("Hello %s!\n", name != NULL ? name : "World");
+    return EXIT_SUCCESS;
+}
